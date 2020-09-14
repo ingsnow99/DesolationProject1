6f928e8f5738202973d299f6be9ad4ed29e12e4b
ZhenLoong <57249291+ZhenLoong@users.noreply.github.com>
2020-09-14 16:26:40 +0800

    3rd commit

---
+++ Assets/Script/UI/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenu;
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
---
+++ Assets/Script/UITween/MenuTween.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class MenuTween : MonoBehaviour
+{
+    [Header("Buttons")]
+    public ButtonTween[] list;
+
+    [Header("Panel")]
+    public GameObject panel;
+    public bool startingPanel;
+
+    [Header("Move Panel Up")]
+    public float upPos;
+    public float upDuration;
+    public Ease upEase;
+
+    [Header("Move Panel Down")]
+    public float downPos;
+    public float downDuration;
+    public Ease downEase;
+
+    [Header("Scale Panel")]
+    public float scaleValue;
+    public float scaleDuration;
+    public Ease scaleEase;
+
+    private void OnEnable()
+    {
+        if (startingPanel)
+        {
+            EnlargeButtons();
+        }
+    }
+
+    public void MoveUp()
+    {
+        panel.SetActive(true);
+        panel.transform.DOLocalMoveY(upPos, upDuration).SetEase(upEase).OnComplete(EnlargeButtons);
+    }
+
+    public void MoveDown()
+    {
+        ShrinkButton();
+        panel.transform.DOLocalMoveY(downPos, downDuration).SetEase(downEase).OnComplete(() => gameObject.SetActive(false));
+    }
+
+    public void ScalePanel()
+    {
+        panel.transform.DOScale(scaleValue, scaleDuration).SetEase(scaleEase);
+    }
+
+    public void EnlargeButtons()
+    {
+        foreach (ButtonTween button in list)
+        {
+            button.EnlargeButton();
+        }
+    }
+
+    public void ShrinkButton()
+    {
+        foreach (ButtonTween button in list)
+        {
+            button.ShrinkButton();
+        }
+    }
+}
