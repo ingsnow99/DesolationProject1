36b4e900f8f8e5fb4697cd41d530c4cef0fee5ad
ZhenLoong <57249291+ZhenLoong@users.noreply.github.com>
2020-09-14 17:24:30 +0800

    4th commit

--- Assets/Script/Drone/DroneMove.cs
+++ Assets/Script/Drone/DroneMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DroneMove : MonoBehaviour
 {
@@ -9,6 +10,17 @@ public class DroneMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Patrol()
+    {
+        for (int i = 0; i < resources.Length; i++)
+        {
+            if (Vector3.Distance(gameObject.transform.position, resources[i].position) == 0)
+            {
+                gameObject.GetComponent<NavMeshAgent>().SetDestination(resources[i+1].position);
+            }
+        }
     }
 }
