2f2762237949159c1ca712836d159617f28a490c
ZhenLoong <57249291+ZhenLoong@users.noreply.github.com>
2020-09-14 21:41:19 +0800

    5th commit

---
+++ Assets/Script/Drone/DroneCollect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DroneCollect : MonoBehaviour
+{
+    int resourceGain,totalNumberOfResource;
+    public Text resourceGainText;
+    public string pickUpTag;
+    public double force; 
+    public float timer;
+    public double defaultPickUpDuration;
+    public double affectRate;
+
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other) {
+        
+        if(other.gameObject.tag==pickUpTag)
+        {
+            //Here should make the drone to stop 
+            gameObject.GetComponent<DroneMove>().isCollecting = true;
+            timer+=Time.deltaTime;
+            float seconds=timer % 60;
+
+            if(seconds> (defaultPickUpDuration-force))
+            {   
+                double realDuration=defaultPickUpDuration-(force*affectRate);
+                Debug.Log("Successful PickUp duration:"+realDuration);
+                Destroy(other.gameObject);
+                resourceGain++;
+                resourceGainText.text= resourceGain +" / "+ totalNumberOfResource;
+                timer=0;
+                // make the drone move again to next waypoint
+                gameObject.GetComponent<DroneMove>().isCollecting = false;
+            }
+
+        }
+    }
+     void Start()
+    {
+        GameObject[] resources;
+        resources=GameObject.FindGameObjectsWithTag(pickUpTag);
+        totalNumberOfResource=resources.Length;
+        resourceGain=0;
+        resourceGainText.text= resourceGain +" / "+ totalNumberOfResource;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+  
+}
--- Assets/Script/Drone/DroneMove.cs
+++ Assets/Script/Drone/DroneMove.cs
@@ -5,22 +5,60 @@ using UnityEngine.AI;
 
 public class DroneMove : MonoBehaviour
 {
-    public Transform[] resources;
+    public List<Transform> resources;
+    public Transform startingPos;
+    public float range;
+    private int currentWaypoint;
+    private bool endPath;
+    private bool beginMove;
+    public bool isCollecting;
+
+    private void Start()
+    {
+        currentWaypoint = 0;
+        endPath = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                resources.Add(hit.transform);
+            }
+        }
+
+        if (beginMove && !isCollecting)
+        { 
+            if (Vector3.Distance(gameObject.transform.position, resources[currentWaypoint].position) < range)
+            {
+                currentWaypoint++;
+                if (currentWaypoint >= resources.Count)
+                {
+                    currentWaypoint = resources.Count - 1;
+                    endPath = true;
+                }
+            }
+
+            if (!endPath)
+            {
+                gameObject.GetComponent<NavMeshAgent>().SetDestination(resources[currentWaypoint].position);
+            }
+            else
+            {
+                gameObject.GetComponent<NavMeshAgent>().SetDestination(startingPos.position);
+            }
+        }
 
     }
 
-    public void Patrol()
+    public void BeginMove()
     {
-        for (int i = 0; i < resources.Length; i++)
-        {
-            if (Vector3.Distance(gameObject.transform.position, resources[i].position) == 0)
-            {
-                gameObject.GetComponent<NavMeshAgent>().SetDestination(resources[i+1].position);
-            }
-        }
+        //button function
+        beginMove = true;
     }
 }
